97a945ed6916cbf8343b52d21d3e7a4e6273c92a
Brandon Ruggles <brandonrninefive@gmail.com>
2016-10-22 22:30:36 -0400

    Created a simple JSON protocol for the server. The server currently handles connecting and disconnecting with nicknames. Also added support for a server config file to read settings from.

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,119 @@
 var WebSocketServer = require('ws').Server;
-var server = new WebSocketServer({port: 8080});
+var fs = require('fs');
 
-server.on('connection', function connection(ws)
+function configExists()
 {
-	ws.on('message', function incoming(message)
+	var exists = true;
+	try
 	{
-		console.log('received: %s', message);
+		fs.accessSync("server.conf");
+	}
+	catch(err)
+	{
+		exists = false;
+	}
+	return exists;
+}
+
+function createConfig()
+{
+	console.log("No server.conf file detected! Generating server.conf...");	
+	try
+	{
+		fs.writeFileSync("server.conf", "Test");
+		console.log("Successfully generated server.conf!");
+	}
+	catch(err)
+	{
+		console.log("Failed to generate server.conf! Reason: " + err);
+	}
+}
+
+function broadcastResponse(connectionList, responseString)
+{
+	connectionList.forEach(function(conn)
+	{
+		conn.connection.send(responseString);								
 	});
-	ws.send('something');
-});
+}
+
+function runServer(portNumber)
+{
+	console.log("Running the IDE server on port " + portNumber + "...");
+	var server = new WebSocketServer({port: portNumber});
+	var connectionList = []; 
+	server.on('connection', function connection(ws)
+	{
+		console.log('New connection attempted!');
+		ws.on('message', function incoming(message)
+		{
+			console.log('received: %s', message);
+			var response = {"type": null, "contents": null};
+			try
+			{
+				var json_message = JSON.parse(message);
+				var nickname = json_message.nickname;
+				var contents = json_message.contents;
+				var command = contents.split(' ')[0].toLowerCase();
+				var spaceIndex = contents.indexOf(' ');
+				var params = contents.substring(spaceIndex + 1, contents.length - command.length);
+				switch(command)
+				{
+					case "connect":
+						response.type = "Connection-Accept";
+						connectionList.forEach(function(conn)
+						{
+							if(conn.nickname == nickname)
+							{
+								response.contents = {"Accepted": false, "Reason": "The nickname you selected is already in use on this server. Please enter a unique nickname and try again."};
+								console.log("Rejected incoming connection for taken nickname.");
+							}
+						});
+						if(response.contents == null)
+						{
+							connectionList.push({"connection":ws,"nickname":nickname});
+							response.contents = {"Accepted": true};
+							console.log("Accepted incoming connection from user '"+ nickname  +"'.");
+						}
+						ws.send(JSON.stringify(response));
+						break;
+					case "message":
+						console.log("Received chat message: " + params);
+						break;
+					default:
+						response.type = "Error";
+						response.contents = "Unrecognized command '" + command  + "'!";
+						break;
+				}
+			}
+			catch(err)
+			{
+				response.type = "Error";
+				response.contents = "The message received did not match the proper protocol!\n Message: " + message + "\nExact error: " + err;
+				console.log(err);
+			}			
+		});
+		ws.on('close', function()
+		{
+			var found = false;
+			connectionList.forEach(function(conn)
+			{
+				if(conn.connection == ws)
+				{
+					found = true;
+					console.log("User '" + conn.nickname  + "' has disconnected!");
+					connectionList.splice(connectionList.indexOf(conn), 1);
+				}
+			});	
+			if(!found)
+			{
+				console.log("An unknown client disconnected!");
+			}					
+		});
+	});
+}
+
+if(!configExists())
+	createConfig();
+
+runServer(8080);
