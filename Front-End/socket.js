f876135909c7427a21a8f5855e7b3df5a4515e96
Jarel <jcp38@njit.edu>
2016-10-22 20:19:02 -0400

    initial realtime update commit

---
+++ Front-End/socket.js
@@ -0,0 +1,28 @@
+function Send()
+{
+
+    var sock = new WebSocket("afsaccess2.njit.edu/~btr2:8001");
+    sock.onopen = function()
+    {
+        sock.send("Connected");
+    };
+
+    sock.send(document.getElementById('code').value);
+    sock.close();
+
+
+}
+ 
+
+function Receive()
+{
+    var sock = new WebSocket("afsaccess2.njit.edu/~btr2:8001");
+
+    sock.onmessage = function(sent){
+        var message = sent;
+        document.getElementById('code').value = message;
+    }
+    sock.close();
+    
+}
+
