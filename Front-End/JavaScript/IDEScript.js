ae22f08e70713dac307ba4fa4bba08475be85f29
John Moreira <jnm26@njit.edu>
2016-10-23 23:00:57 -0400

    initial commit of Login page, IDE page, JavaScript for IDE, and CSS for both

---
+++ Front-End/JavaScript/IDEScript.js
@@ -0,0 +1,45 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function showFileDropdown() {
+    document.getElementById("fileDropdown").classList.toggle("show");
+}
+
+function showEditDropdown() {
+    document.getElementById("editDropdown").classList.toggle("show");
+}
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
