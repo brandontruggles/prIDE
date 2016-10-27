a4fee8bcc39d3f43e6f067ad27c1b9497ddc73b9
Jarel <jcp38@njit.edu>
2016-10-27 13:01:13 -0400

    remake methods

--- Front-End/js/socket.js
+++ Front-End/js/socket.js
@@ -3,6 +3,7 @@ var nickname;
 var ntabs = 1;
 var currfile;
 var currproject;
+var name;
 function Connection()//works
 {
 
@@ -19,22 +20,117 @@ function Connection()//works
 
     };
     sock.onmessage = function(response){
+
         var res = JSON.parse(response.data);
         var contents = res.contents;
-        if(contents.Accepted){
-            alert("Connected");
-            //nickname = document.getElementById('nickname').value;
-            //document.location.href = "IDEMain.html";
-        }
-        else{
-          port = prompt("Enter port");
-          nickname = prompt("Enter nickname");
-          sock.close();
-          sock = new WebSocket("ws://45.55.218.73:"+port);
-        }
-    }
-}
+        switch(res.type){
+          case "Connection-Accept":
+            if(contents.Accepted){
+                //nickname = document.getElementById('nickname').value;
+                //document.location.href = "IDEMain.html";
+            }
+            else{
+              alert(contents.Reason);
+              port = prompt("Enter port");
+              nickname = prompt("Enter nickname");
+              sock.close();
+              sock = new WebSocket("ws://45.55.218.73:"+port);
+            }
+            break;
+          case "Compile-Running-Status":
+            var res = JSON.parse(response.data);
+            break;
+          case "Code-Running-Status":
+            break;//needs code
+          case "Message-Broadcast":
+            document.getElementById('consoleWindow').innerHTML += contents+"\n";
+            break;
+          case "Project-Created-Status":
+            if(contents.Created){
+              alert("new project created");
+              var fileList = document.getElementById('openproj');
+              fileList.innerHTML += '<li><a href="#">'+name+'/</a></li>';
+              currproject = name;
 
+
+            }
+            else{
+              alert(contents.Reason);
+            }
+            break;
+          case "File-Created-Status":
+            var numOfFiles = 0;
+            if(contents.Created){
+              alert("new file created");
+              currfile = name+'.java';
+
+          var textareas = document.getElementById("textareas");
+          textareas.innerHTML += "<div id=\"class"+ntabs+"\" class=\"tabcontent\">\n\
+      <ul id='openproj'>\n\
+      <li>Solution Explorer</li>\n\
+      </ul>\n\
+      \n\
+      \n\
+      <textarea rows=\"10\" cols=\"25\" id=\"codespace\" onkeydown=\"Update()\">\n\
+      public class "+currfile+"\n\
+      {\n\
+        public static void main(String[] args)\n\
+        {\n\
+          //Your Code Here\n\
+        }\n\
+      }</textarea>\n\
+      <textarea placeholder=\"Console\" id=\"consoleWindow\" rows=\"20\" cols=\"25\"></textarea>\n\
+      </div>\n\
+      ";
+            var fileList = document.getElementById('openproj');
+          //if(num)
+          //var tab1 = document.getElementById('tab1');
+              fileList.innerHTML += '<li><a href="#">'+name+'</a></li>';
+
+          var tabList = document.getElementById('tabs');
+          //var class4 = document.getElementById('class4');
+          //class4String = '
+          //tab1.innerHTML = name;
+          tabList.innerHTML += '<li><a href="javascript:void(0)" class="tablinks" id="tab'+ntabs+'" onclick="openTab(event, \'class'+ntabs+'\')">'+name+'</a></li>';
+          }
+            else{
+              alert(contents.Reason);
+            }
+          break;
+        case "Directory-Created-Status":
+          if(contents.Created){
+            alert("directory created");
+            var fileList = document.getElementById('openproj');
+            fileList.innerHTML += '<li><a href="#">'+name+'/</a></li>';
+          }
+          else{
+            alert(contents.Reason);
+          }
+          break;
+        case "Project-Open-Response":
+          if(contents.Opened){
+            var fileList = document.getElementById('openproj');
+            fileList.innerHTML = '';//empty out file explorer
+            for(var i = 0; i < contents.Files.length; i++){
+              fileList.innerHTML += '<li><a href="#">'+contents.Files[i]+'</a></li>';
+            }
+
+
+          }
+          else{
+            alert("no projects make one");
+          }
+          break;
+        case "File-Update-Response":
+          document.getElementById('codespace').value = message;
+          break;
+
+        default:
+          alert("dam son");
+          break;
+      }//switch
+    }//onmessage
+}
 
 
 function Update()
@@ -44,11 +140,11 @@ function Update()
         "contents": "updatefile "+currfile+" "+document.getElementById('codespace').value
     };
     sock.send(JSON.stringify(message));
-    sock.onmessage = function(response){
+    /*sock.onmessage = function(response){
         var res = JSON.parse(response.data);
         var message = res.contents;
         document.getElementById('code').value = message;
-    }
+    }*/
 
 }
 
@@ -60,23 +156,19 @@ function compile()//hold on for alec
     }
     sock.send(JSON.stringify(message));
 
-    sock.onmessage = function (response) {
-      var res = JSON.parse(response.data);
-
-    }
 }
 
 
 function newproject()//works
 {
-    var name = prompt("name of project");
+    name = prompt("name of project");
     var message = {
         "nickname": nickname,
         "contents": "newproject "+name
     }
     sock.send(JSON.stringify(message));
 
-    sock.onmessage = function(response){
+    /*sock.onmessage = function(response){
       var res = JSON.parse(response.data);
       var contents = res.contents;
       if(contents.Created){
@@ -90,7 +182,7 @@ function newproject()//works
       else{
         alert(contents.Reason);
       }
-    }
+    }*/
 }
 
 function newfile()//works
@@ -102,7 +194,7 @@ function newfile()//works
         "contents": "newfile "+name
     }
     sock.send(JSON.stringify(message));
-    sock.onmessage = function(response){
+    /*sock.onmessage = function(response){
       var res = JSON.parse(response.data);
       var contents = res.contents;
 	  var numOfFiles = 0;
@@ -142,23 +234,20 @@ public class Test "+ntabs+"\n\
       else{
         alert(contents.Reason);
       }
-    }
+    }*/
 }
 
 function message()
 {//for chat
     var message = {
         "nickname": nickname,
-        "contents": "message "+document.getElementById('chat').value
+        "contents": "message "+document.getElementById('commandArea').value
     }
     sock.send(JSON.stringify(message));
 
-    sock.onmessage = function (response) {
-      var res = JSON.parse(response.data);
-      alert(res.contents);
-
-    }
 }
+
+
 
 function newdir()//works
 {
@@ -168,7 +257,7 @@ function newdir()//works
         "contents": "newdir "+ name
     }
     sock.send(JSON.stringify(message));
-    sock.onmessage = function(response){
+    /*sock.onmessage = function(response){
       var res = JSON.parse(response.data);
       var contents = res.contents;
       if(contents.Created){
@@ -179,7 +268,7 @@ function newdir()//works
       else{
         alert(contents.Reason);
       }
-    }
+    }*/
 }
 
 function openproject()//works
@@ -189,7 +278,7 @@ function openproject()//works
         "contents": "openproject"
     }
     sock.send(JSON.stringify(message));
-    sock.onmessage = function(response){
+    /*sock.onmessage = function(response){
         var res = JSON.parse(response.data);
         var contents = res.contents;
         if(contents.Opened){
@@ -205,7 +294,7 @@ function openproject()//works
           alert("no projects make one");
         }
 
-    }
+    }*/
 
 }
 
