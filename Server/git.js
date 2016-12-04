38ba81f5f75a4047236dd4e6c371ac199d571f4a
Brandon Ruggles <brandonrninefive@gmail.com>
2016-12-03 21:06:39 -0500

    Added new modules for git and the IDE file system.

---
+++ Server/git.js
@@ -0,0 +1,74 @@
+module.exports = 
+{
+	runGit:function(params, dir)
+	{
+		execFileSync("git", params.split(' '), {"cwd": "workspace/" + dir});
+	},
+	createproj:function(user, pass, name)
+	{
+		var out = execFileSync("curl", ["-i", "-u", user + ":" + pass, "-d", "\'{\"name\":\"" + name + "\"}\'", "-X", "POST", "https://api.github.com/user/repos"]);
+
+		var obj = JSON.parse(out);
+		return out;
+	},
+	testlogin:function(user, pass)
+	{
+		var out = execFileSync("curl", ["-u", user + ":" + pass, "https://api.github.com"]);
+
+		var obj = JSON.parse(out);
+		if (obj.message)
+			return true;
+		return false;
+	},
+	clone:function(url)
+	{
+		var out = execFileSync("git", ["clone", url], {"cwd": "workspace"}).toString();
+		console.log(out);
+		return out;
+	},	
+	pull:function(dir)
+	{
+		var out = execFileSync("git", ["pull"], {"cwd": "workspace/" + dir});
+		console.log(out.toString());
+		return out.toString();
+	},
+	add:function(filename, dir)
+	{
+		var out = "";
+		try
+		{
+			out = execFileSync("git", ["add", filename], {"cwd": "workspace/" + dir});
+		}
+		catch (e)
+		{
+			out =  e.message.toString();
+		}
+		return out;
+	},
+	commit:function(message, dir)
+	{
+		var out = "";
+		try
+		{
+			out = execFileSync("git", ["commit", "-am", message], {"cwd": "workspace/" + dir});
+		}
+		catch (e)
+		{
+			out = e.message.toString();
+		}
+		return out;
+	},
+	push:function(dir)
+	{
+		var out = "";
+		try
+		{
+			out = execFileSync("git", ["push", "origin", "master"], {"cwd": "workspace/" + dir});
+		}
+		catch (e)
+		{
+			out = e.message.toString();
+		}
+		return out;
+	}
+};
---
+++ Server/ideFS.js
@@ -0,0 +1,105 @@
+var fs = require('fs');
+var configObj = {};
+module.exports = 
+{
+	getConfigObj:function()
+	{
+		return configObj;
+	},
+	configExists:function()
+	{
+		var exists = true;
+		try
+		{
+			fs.accessSync("server.conf");
+		}
+		catch(err)
+		{
+			exists = false;
+		}
+		return exists;
+	},
+	createConfig:function()
+	{
+		console.log("No server.conf file detected! Generating server.conf...");
+		try
+		{
+			fs.writeFileSync("server.conf", "{\n\t\"port\": 8080,\n\t\"max_clients\": 8\n}");
+			console.log("Successfully generated server.conf!");
+		}
+		catch(err)
+		{
+			console.log("Failed to generate server.conf! Reason: " + err);
+		}
+	},
+	readConfig:function()
+	{
+		try
+		{
+			configObj = JSON.parse(fs.readFileSync("server.conf", "utf8").toString());
+		}
+		catch(err)
+		{
+			console.log("Failed to read server.conf! Reason: " + err);
+		}
+	},
+	writeConfig:function()
+	{
+		try
+		{
+			fs.writeFileSync("server.conf", JSON.stringify(configObj, null, "\t"));
+		}
+		catch(err)
+		{
+			console.log("Failed to write server.conf! Reason: " + err);
+		}
+	},      	
+	newfile:function(filename, dir)
+	{
+		var out = "";
+		try
+		{
+			out = execFileSync("touch", [filename], {"cwd": "workspace/" + dir});
+		}
+		catch (e)
+		{
+			out = e.message.toString();
+		}
+		return out;
+	},
+	compile:function(dir)
+	{
+		var files = getProjectFiles(dir);
+		var flies = [];
+		for (var i = 0; i < files.length; i++)
+		{
+			if (files[i].substr(files[i].length - 5) == ".java")
+			{
+				flies.push("workspace/" + dir + "/" + files[i]);
+			}
+		}
+		try
+		{
+			var ret = execFileSync("javac", flies, {stdio: ['pipe', 'pipe', 'pipe']}).toString();
+			return ret;
+		}
+		catch (error)
+		{
+			return error.message;
+		}
+	},
+	run:function(prog, args, dir)
+	{
+		prog = prog.replace(".java","");
+		var str = execFileSync("java", ["-cp", "workspace/" + dir, prog]).toString();
+		return str;
+	},
+	listproj:function(user)
+	{
+		execFileSync("curl https://api.github.com/users/" + user + "/repos", writeout);
+		out = fs.readFileSync("stdout.txt", "utf8").toString();
+		var obj = JSON.parse(out);
+
+		return obj;
+	}
+};
