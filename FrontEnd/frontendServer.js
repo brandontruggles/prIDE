554a14e961b022397892511fafe0a152c6a50fbd
ashanren <jcp38@njit.edu>
2017-03-02 18:57:09 -0500

    Finished sorting npm and ExpressJS is fully operational. npm start now to start prIDE

--- FrontEnd/frontendServer.js
+++ FrontEnd/frontendServer.js
@@ -7,16 +7,16 @@ var app = express();
 app.use('/image',express.static(__dirname+'/image'));
 app.use('/css', express.static(__dirname+'/css'));
 app.use('/js', express.static(__dirname+'/js'));
-app.use('/node_modules', express.static(__dirname+'/node_modules'));
+app.use('/node_modules', express.static(path.join(__dirname, '../', 'node_modules')));
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
-app.listen(9000, 'localhost', function (err){ 
+app.listen(9001, '0.0.0.0', function (err){ 
   if(err){
     return console.error(err);
   }
-  console.log('listening at http://localhost:9000');
-  opn('http://localhost:9000');
+  console.log('listening at http://localhost:9001');
+  opn('http://localhost:9001');
 })
