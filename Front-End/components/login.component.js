a3139c88b5282039b4fd5658e5e0782b0f273091
null <null>
2017-03-31 00:35:18 +0000

    basic login screen UI now up using React - no functionality yet

--- Front-End/components/login.component.js
+++ Front-End/components/login.component.js
@@ -5,8 +5,12 @@ class Login extends React.Component {
   render(){
     return(
       <div id="loader">
-      <input type="text" id="nick" placeholder="NickName" /><br />
-      <input type="button" id="firstSubmit" value="Submit" />
+	<img id="logo" src="image/pridefull.png" alt="Logo"/>
+	<input type="text" class="firstInput" id="firstInput" placeholder="Nickname" autofocus onkeydown="fn.chatkeydown(this)"/><br/>
+	<input type="button" id="firstSubmit" value="Submit" onclick="Connection()"/>
+	<br/><br/>
+<input type="button" id="changebackground" value="Background"/>
+	<a href="https://github.com/brandonrninefive/prIDE" id="githublink">prIDE on GitHub</a>
       </div>
     )
 
